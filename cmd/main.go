e99545dacf5ab472cd3cee924512b2e45878ad69
benkoimet@gmail.com <koimettb@gmail.com>
2026-04-27 11:52:26 +0300

    feat: add login, register and home handlers

--- cmd/main.go
+++ cmd/main.go
@@ -7,12 +7,44 @@ import (
 	
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello, the server appears to be on"))
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w,r)
+	} else {
+	w.Write([]byte(`
+		<h1></h1>
+		<a href="/login">Login</a> | <a href="/register">Register</a>	
+	`))
+	}
+
+
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`
+        <form action="/login" method="POST">
+            <input type="text" placeholder="Email" name="email">
+            <input type="password" placeholder="Password" name="password">
+            <button type="submit">Login</button>
+        </form>		
+	`))
+}
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte(`
+        <form action="/register" method="POST">
+            <input type="text" placeholder="Username" name="username">
+            <input type="email" placeholder="Email" name="email">
+            <input type="password" placeholder="Password" name="password">
+            <button type="submit">Register</button>
+        </form>
+    `))
 }
 
 func main() {
-	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/register", registerHandler)
 
 	fmt.Println("server running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
